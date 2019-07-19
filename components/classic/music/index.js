7d3b6a099ef06c563b9e5f96c9aa5c4dd637ff06
phelps <phelps@phelpsdeMacBook-Pro.local>
2019-07-20 00:47:42 +0800

    music cpn finish

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -20,6 +20,10 @@ Component({
     pauseSrc:'images/player@pause.png',
     playSrc:'images/player@play.png'
   },
+  attached:function (e) { 
+    this._recoverStatus(),
+    this._monitorSwtich()
+   },
 
   /**
    * 组件的方法列表
@@ -39,6 +43,34 @@ Component({
         })
         mMgr.pause()
       }
-     }
+     },
+     _recoverStatus:function () { 
+        if(mMgr.paused){
+          this.setData({
+            playing:false
+          })
+          return
+        }
+        if(mMgr.src == this.properties.src){
+          this.setData({
+            playing:true
+          })
+        }
+      },
+      _monitorSwtich:function () { 
+        mMgr.onPlay( ()=>{
+          this._recoverStatus()
+        })
+        mMgr.onPause( ()=>{
+          this._recoverStatus()
+        })
+        mMgr.onStop( ()=>{
+          this._recoverStatus()
+        })
+        mMgr.onEnded( ()=>{
+          this._recoverStatus()
+        })
+       }
+      
   }
 })
